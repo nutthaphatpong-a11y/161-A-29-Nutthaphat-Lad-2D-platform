6a8157e7ebefaa26ab55de9a8f9aa71122ac6cbd
nutthaphatpong <nutthaphat.pong@bumail.net>
2025-11-04 03:27:24 +0700

    Bullet

---
+++ Assets/Scripts/Banana.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Banana : Weapon
+{
+    public override void Move()
+    {
+
+    }
+
+    public override void OnHitWith(Character character)
+    {
+
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,27 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player : Character
+public class Player : Character, IShootable
+
 {
+    [field: SerializeField] public GameObject Bullet { get; set; }
+
+    [field: SerializeField] public Transform ShootPoint { get; set; }
+
+    public float ReloadTime { get; set; }
+
+    public float WaitTime { get; set; }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         base.Initialize(100);
+        ReloadTime = 1.0f;
+        WaitTime = 0.0f;
+    }
+
+    private void FixedUpdate()
+    {
+        WaitTime += Time.fixedDeltaTime;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -23,9 +39,24 @@ public class Player : Character
     {
         TakeDamage(enemy.DamageHit);
     }
-    // Update is called once per frame
-    void Update()
+
+    public void Shoot()
     {
-        
+        if (Input.GetButtonDown("Fire1") && WaitTime >= ReloadTime)
+        {
+            var bullet = Instantiate(Bullet, ShootPoint.position, Quaternion.identity);
+            Banana banana = bullet.GetComponent<Banana>();
+            if (banana != null)
+                banana.InitWeapon(20, this);
+
+            WaitTime = 0f; // reset timer หลังยิง
+        }
+    }
+   
+
+    private void Update()
+    {
+        Shoot();
     }
 }
+
